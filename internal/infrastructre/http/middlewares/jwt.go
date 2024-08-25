e964e24c54e0a17bc75654a9c0d7cd08bd2c7dfa
Sayuru Piyasooriya <sayuru@sekahost.co.uk>
2024-08-25 18:12:20 +0530

    chore(jwt): jwt setup

---
+++ internal/infrastructre/http/middlewares/jwt.go
@@ -0,0 +1,43 @@
+package middlewares
+
+import (
+	"fmt"
+	"github.com/dgrijalva/jwt-go"
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+func JWTAuthentication(secretKey string) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		tokenString, err := c.Cookie("jwt")
+		if err != nil {
+			c.JSON(http.StatusUnauthorized, gin.H{"error": fmt.Sprintf("error getting authorization token %s", err)})
+			c.Abort()
+			return
+		}
+
+		if tokenString == "" {
+			c.JSON(http.StatusUnauthorized, gin.H{"error": "authorization is missing"})
+			c.Abort()
+			return
+		}
+
+		token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+			return []byte(secretKey), nil
+		})
+		if err != nil {
+			panic(err)
+		}
+
+		if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
+			// Set custom claims in context
+			c.Set("user_id", claims["sub"])
+			c.Set("role", claims["role"])
+			c.Next()
+		} else {
+			c.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid token"})
+			c.Abort()
+			return
+		}
+	}
+}
--- internal/infrastructre/http/routes/routes.go
+++ internal/infrastructre/http/routes/routes.go
@@ -3,11 +3,25 @@ package routes
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/snpiyasooriya/construction_design_api/internal/infrastructre/http/controllers"
+	"github.com/snpiyasooriya/construction_design_api/internal/infrastructre/http/middlewares"
 )
 
-func InitRoutes(router *gin.Engine, userController *controllers.UserController) {
+func InitRoutes(router *gin.Engine, userController *controllers.UserController, authenticationController *controllers.AuthenticationController) {
 	router.GET("/ping", controllers.Ping)
-	RegisterUserRoutes(router, userController)
+	router.POST("/login", authenticationController.Login)
 
-	// Add more routes as needed
+	// Apply JWT authentication middleware globally
+	secretKey := "ct_sys_api_root"
+	router.Use(middlewares.JWTAuthentication(secretKey))
+	router.Use(middlewares.CabinMiddleware())
+	protectedRoutes := router.Group("/")
+	{
+		userRoutes := protectedRoutes.Group("/user")
+		{
+			userRoutes.POST("/", userController.CreateUser)
+		}
+		_ = protectedRoutes.Group("/authentication")
+		{
+		}
+	}
 }
