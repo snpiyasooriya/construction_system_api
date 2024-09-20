63c6f6b6db5008e7ea62c5ea402fd087db5aae17
Sayuru Piyasooriya <sayuru@sekahost.co.uk>
2024-09-20 10:29:46 +0530

    🦄 refactor(main):

--- main.go
+++ main.go
@@ -2,13 +2,11 @@ package main
 
 import (
 	"github.com/snpiyasooriya/construction_design_api/config"
-	interfaces2 "github.com/snpiyasooriya/construction_design_api/internal/domain/interfaces"
 	"github.com/snpiyasooriya/construction_design_api/internal/infrastructre/database"
 	"github.com/snpiyasooriya/construction_design_api/internal/infrastructre/database/migrations"
 	"github.com/snpiyasooriya/construction_design_api/internal/infrastructre/database/repository"
 	"github.com/snpiyasooriya/construction_design_api/internal/infrastructre/http/controllers"
 	server2 "github.com/snpiyasooriya/construction_design_api/internal/infrastructre/http/server"
-	"github.com/snpiyasooriya/construction_design_api/internal/interfaces"
 	"github.com/snpiyasooriya/construction_design_api/internal/services"
 	"github.com/snpiyasooriya/construction_design_api/internal/usecases"
 )
@@ -18,8 +16,7 @@ func main() {
 	conf := config.GetConfig()
 	db := database.NewPostgres(conf).GetDb()
 	migrations.AutoMigrate(db)
-	var userRepo interfaces2.UserRepository
-	userRepo = repository.NewGormUserRepository(db)
+	userRepo := repository.NewGormUserRepository(db)
 	userCreateUseCase := usecases.NewUserCreateUseCase(userRepo)
 	userController := controllers.NewUserController(userCreateUseCase)
 
@@ -36,8 +33,7 @@ func main() {
 	//projectTypeRepo = repository.NewProjectTypeGORMRepository(db)
 	//projectTypeCreateUseCase := usecases.NewProjectTypeCreateUseCase(projectTypeRepo)
 	//projectTypeController := controllers.NewProjectTypeController(projectTypeCreateUseCase)
-	var server interfaces.Server
-	server = server2.NewGinServer(conf, userController, authenticationController, projectController)
+	server := server2.NewGinServer(conf, userController, authenticationController, projectController)
 	server.Start()
 
 }
