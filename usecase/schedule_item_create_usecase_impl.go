6d88dc12d9a13ece0a7763ceb45064c683c4ab50
Sayuru Piyasooriya <sayuru@seekahost.co.uk>
2025-07-23 23:15:45 +0530

    fix(schedule):enter shape values

--- usecase/schedule_item_create_usecase_impl.go
+++ usecase/schedule_item_create_usecase_impl.go
@@ -5,7 +5,6 @@ import (
 
 	"github.com/snpiyasooriya/construction_design_api/dto"
 	"github.com/snpiyasooriya/construction_design_api/interfaces/repository"
-	"github.com/snpiyasooriya/construction_design_api/pkg/utils"
 )
 
 type ScheduleItemCreateUseCaseImpl struct {
@@ -44,8 +43,6 @@ func (s *ScheduleItemCreateUseCaseImpl) Execute(scheduleItemCreateInputDTO dto.S
 	if shape == nil {
 		return nil, errors.New("shape not found")
 	}
-
-	scheduleItemCreateInputDTO.ShapeDimensions = utils.InterfaceToDataTypeJson(shape.Labels)
 
 	// Create the schedule item
 	scheduleItemOutput, err := s.scheduleItemRepo.Create(scheduleItemCreateInputDTO)
